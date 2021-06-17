0a526e50b3200cc109d1f10c664a3c0fc012a4c1
Tho Ho <tho.ho@theenergycompany.sg>
2021-06-17 17:51:39 +0800

    Add Project files.

---
+++ EchoServiceApi/Controllers/EchoController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+
+namespace EchoServiceApi.Controllers
+{
+    [Route("e")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public EchoController(ILogger<EchoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("400")]
+        public IActionResult OnError400() => CodeAndMessage(400, "Test bad request");
+
+        [Route("401")]
+        public IActionResult OnError401() => CodeAndMessage(401, "Test unauthorized");
+
+        [Route("403")]
+        public IActionResult OnError403() => CodeAndMessage(403, "Test forbidden");
+
+        [Route("404")]
+        public IActionResult OnError404() => CodeAndMessage(404, "Test not found");
+
+        [Route("500")]
+        public IActionResult OnError500() => CodeAndMessage(500, "Test internal server error");
+
+        [Route("502")]
+        public IActionResult OnError502() => CodeAndMessage(502, "Test bad gateway");
+
+        [Route("503")]
+        public IActionResult OnError503() => CodeAndMessage(503, "Test service unavailable");
+
+        private IActionResult CodeAndMessage(int code, string message) => new ContentResult
+        {
+            Content = message,
+            ContentType = "text/plain",
+            StatusCode = code,
+        };
+
+        [Route("{*url}")]
+        public async Task<ActionResult<MyResult>> Action(string url)
+        {
+            var request = HttpContext.Request;
+            var result = new MyResult
+            {
+                Url = url,
+                Method = request.Method,
+                Scheme = request.Scheme,
+                Host = request.Host.ToString(),
+                Connection = new ConnInfo(HttpContext.Connection),
+                PathBase = request.PathBase.ToString(),
+                Path = request.Path.ToString(),
+                ContentType = request.ContentType,
+                QueryString = request.QueryString.ToString(),
+                Headers = ToDict1(request.Headers),
+                Form = ToDict1(request.HasFormContentType ? request.Form : null),
+                Query = ToDict1(request.Query),
+            };
+
+            if (!string.IsNullOrEmpty(request.ContentType))
+            {
+                using (var reader = new System.IO.StreamReader(Request.Body, System.Text.Encoding.UTF8))
+                {
+                    result.Body = await reader.ReadToEndAsync();
+                }
+            }
+
+            return result;
+        }
+
+        private IDictionary<string, string> ToDict1(IEnumerable<KeyValuePair<string, StringValues>> headers)
+        {
+            return headers?.ToDictionary(e => e.Key, e => e.Value.ToString());
+        }
+
+        public class MyResult
+        {
+            public string Url { get; set; }
+            public string Body { get; set; }
+            public string Method { get; set; }
+            public string Scheme { get; set; }
+            public string Host { get; set; }
+            public ConnInfo Connection { get; set; }
+            public string PathBase { get; set; }
+            public string Path { get; set; }
+            public string ContentType { get; set; }
+            public string QueryString { get; set; }
+            public IDictionary<string, string> Headers { get; set; }
+            public IDictionary<string, string> Form { get; set; }
+            public IDictionary<string, string> Query { get; set; }
+        }
+
+        public class ConnInfo
+        {
+            public ConnInfo(ConnectionInfo connectionInfo)
+            {
+                if (connectionInfo != null)
+                {
+                    Id = connectionInfo.Id;
+                    LocalIpAddress = connectionInfo.LocalIpAddress?.ToString();
+                    LocalPort = connectionInfo.LocalPort;
+                    RemoteIpAddress = connectionInfo.RemoteIpAddress?.ToString();
+                    RemotePort = connectionInfo.RemotePort;
+                }
+            }
+
+            public string Id { get; }
+            public string LocalIpAddress { get; }
+            public int LocalPort { get; }
+            public string RemoteIpAddress { get; }
+            public int RemotePort { get; }
+        }
+    }
+}
---
+++ EchoServiceApi/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetLah.Diagnostics;
+using NetLah.Extensions.Logging;
+
+namespace EchoServiceApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            AppLog.Logger.LogInformation("Startup ...");
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            AppLog.Logger.LogInformation("ConfigureServices ...");
+            services.AddControllers();
+            services.AddHealthChecks();     // Registers health checks services
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, IHostApplicationLifetime applicationLifetime)
+        {
+            var logger = AppLog.Logger;
+            logger.LogInformation("WebApplication configure ...");
+            logger.LogInformation("Environment: {environmentName}; DeveloperMode:{isDevelopment}", env.EnvironmentName, env.IsDevelopment());
+
+#pragma warning disable S3923 // All branches in a conditional structure should not have exactly the same implementation
+            if (env.IsDevelopment())
+#pragma warning restore S3923 // All branches in a conditional structure should not have exactly the same implementation
+            {
+                // app.UseDeveloperExceptionPage()
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts()
+            }
+
+            app.UseSerilogRequestLoggingLevel(LogLevel.Information);
+
+            app.UseHealthChecks("/healthz");
+            //app.UseHttpsRedirection()
+            app.UseStatusCodePages();
+            //app.UseStatusCodePagesWithReExecute("/ErrorPages/{0}.html")
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                var appInfo = ApplicationInfo.Instance ?? ApplicationInfo.Initialize(null);
+                logger.LogInformation("App:{title}; Version:{version} BuildTime:{buildTime}; Framework:{framework}",
+                    appInfo.Title, appInfo.InformationalVersion, appInfo.BuildTimestampLocal, appInfo.FrameworkName);
+            });
+        }
+    }
+}
