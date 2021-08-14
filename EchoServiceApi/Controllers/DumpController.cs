0d91b3a9b64be3236e307dbef70486752fba452a
Tho Ho <tho.ho@theenergycompany.sg>
2021-08-14 21:53:55 +0800

    dump appSettings?env=1; dump connectionStrings; dump environments

---
+++ EchoServiceApi/Controllers/DumpController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NetLah.Extensions.Configuration;
+
+namespace EchoServiceApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class DumpController : ControllerBase
+    {
+        private static IDictionary<string, string> GetEnvironmentVariables()
+        {
+            var result = new Dictionary<string, string>();
+            foreach (System.Collections.DictionaryEntry item in Environment.GetEnvironmentVariables())
+            {
+                result.Add((string)item.Key, (string)item.Value);
+            }
+            return result;
+        }
+
+        public JsonResult AppSettings([FromServices] IConfiguration configuration, string env)
+        {
+            var environmentVariables = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            if (string.IsNullOrEmpty(env) || !new[] { "true", "t", "yes", "y", "1" }.Contains(env.Trim(), StringComparer.OrdinalIgnoreCase))
+            {
+                environmentVariables = GetEnvironmentVariables()
+                    .Select(kv => $"{kv.Key}-{kv.Value}")
+                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+            }
+
+            return new(
+                configuration
+                    .AsEnumerable(true)
+                    .Where(kv => kv.Value != null && !environmentVariables.Contains($"{kv.Key}-{kv.Value}"))
+                    .OrderBy(kv => kv.Key)
+                    .ToDictionary(kv => kv.Key, kv => kv.Value));
+        }
+
+        public JsonResult ConnectionStrings([FromServices] IConfiguration configuration) => new(
+            new ConnectionStringsHelper(configuration)
+                .ConnectionStrings
+                .ToDictionary(c => c.Key, c => new
+                {
+                    c.Value.ConnectionString,
+                    Provider = c.Value.Provider.ToString(),
+                    c.Value.Custom,
+                }));
+
+        public JsonResult Environments()
+        {
+            return new(
+                GetEnvironmentVariables()
+                    .OrderBy(kv => kv.Key)
+                    .ToDictionary(kv => kv.Key, kv => kv.Value));
+        }
+    }
+}
